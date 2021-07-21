a97d7e8ce57a174b497aae3b5f510d98a4f86fb1
Joshua Jones <joshua@general-metrics.com>
2021-07-21 14:11:30 -0700

    Add a placeholder info button

--- src/nextapp/components/activity-list/activity-item.tsx
+++ src/nextapp/components/activity-list/activity-item.tsx
@@ -1,12 +1,9 @@
 import * as React from 'react';
 import {
-  Avatar,
-  Badge,
   Box,
-  Divider,
   Flex,
-  Grid,
-  Heading,
+  Icon,
+  IconButton,
   Tag,
   Text,
   VStack,
@@ -15,6 +12,7 @@ import format from 'date-fns/format';
 
 import ActivityIcon from './activity-icon';
 import { Activity } from '@/shared/types/query.types';
+import { FaInfoCircle } from 'react-icons/fa';
 
 interface ActivityItemProps {
   data: Activity;
@@ -24,40 +22,48 @@ const ActivityItem: React.FC<ActivityItemProps> = ({ data }) => {
   return (
     <Flex align="center" p={2} bgColor="white">
       <ActivityIcon result={data.result} />
-      <VStack align="left" mx={2}>
-        <Flex align="center">
-          <Text fontSize="sm">
-            {data.actor && (
-              <Text
+      <VStack align="stretch" mx={2}>
+        <Flex align="center" justify="space-between" flex={1}>
+          <Box>
+            <Text fontSize="sm">
+              {data.actor && (
+                <Text
+                  as="span"
+                  pr={1}
+                  color="#4183c4"
+                  fontWeight={600}
+                  whiteSpace="nowrap"
+                  textOverflow="ellipsis"
+                  overflow="hidden"
+                >
+                  {data.actor.name}
+                </Text>
+              )}
+              <Tag
                 as="span"
-                pr={1}
-                color="#4183c4"
-                fontWeight={600}
-                whiteSpace="nowrap"
-                textOverflow="ellipsis"
-                overflow="hidden"
+                mr={2}
+                fontWeight="bold"
+                textTransform="uppercase"
+                colorScheme="yellow"
+                size="sm"
               >
-                {data.actor.name}
-              </Text>
-            )}
-            <Tag
-              as="span"
-              mr={2}
-              fontWeight="bold"
-              textTransform="uppercase"
-              colorScheme="yellow"
-              size="sm"
-            >
-              {data.action}
-            </Tag>
-            <Text as="span">{data.message}</Text>
-            <Text
-              as="time"
-              pl={2}
-              fontSize="0.8em"
-              color="rgba(0,0,0,.4)"
-            >{`${format(new Date(data.createdAt), 'HH:mm')} `}</Text>
-          </Text>
+                {data.action}
+              </Tag>
+              <Text as="span">{data.message}</Text>
+              {data.type === 'GatewayConfig' && !data.message && (
+                <Text as="span">Configuration change</Text>
+              )}
+              <Text
+                as="time"
+                pl={2}
+                fontSize="0.8em"
+                color="rgba(0,0,0,.4)"
+              >{`${format(new Date(data.createdAt), 'HH:mm')} `}</Text>
+            </Text>
+          </Box>
+          <IconButton aria-label="View info" variant="ghost" colorScheme="cyan">
+            <Icon as={FaInfoCircle} />
+          </IconButton>
         </Flex>
       </VStack>
     </Flex>
